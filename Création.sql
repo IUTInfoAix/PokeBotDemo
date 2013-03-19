104a426dfa50f0d0bf1c16ed76e85e310d49040e
Change Me <change-me@example.com>
2013-03-19 10:46:11 +0100

    Ajout des DAOs et de la class Owner

--- Création.sql
+++ Création.sql
@@ -1,6 +1,7 @@
   CREATE TABLE OWNER 
    (	PRENOM VARCHAR(20), 
-	POKEMON VARCHAR(20)
+	POKEMON VARCHAR(20),
+	PRIMARY_KEY (POKEMON)
    ) ;
 
 insert into OWNER VALUES ("@CloudDeLuna", "@GwenGoupix"); 
