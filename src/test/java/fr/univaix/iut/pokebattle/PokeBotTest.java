1de12c018d92c779c5e17cbf7b552006c7f83d56
Change Me <change-me@example.com>
2013-03-20 09:29:17 +0100

    test

--- src/test/java/fr/univaix/iut/pokebattle/PokeBotTest.java
+++ src/test/java/fr/univaix/iut/pokebattle/PokeBotTest.java
@@ -1,8 +1,16 @@
 package fr.univaix.iut.pokebattle;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.Test;
+
+import DAO.DAOFactory;
+import DAO.DAOOwner;
+import beans.Owner;
 
 /**
  * Integration tests checking the PokeBot
@@ -16,5 +24,20 @@ public class PokeBotTest {
     public void testSalut() {
         assertEquals("@azaz Pika pika", pokeBot.ask(new Tweet("azaz","Salut")));
         assertEquals("@azaz Pika pika", pokeBot.ask(new Tweet("azaz","This is not a question.")));
+    }
+    
+    @Test
+    public void testOwner() {
+        Owner monOwner = new Owner("Pika","John");
+        System.out.println(monOwner);
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Pokemon");
+        EntityManager em = emf.createEntityManager();
+        DAOFactory daof = new DAOFactory(em);
+        DAOOwner daoOwn = daof.createDAOOwner();
+        
+        Owner UnAutreOwner = daoOwn.getByPokemon("@GwenGoupix");
+        System.out.println(UnAutreOwner);
+        
     }
 }
